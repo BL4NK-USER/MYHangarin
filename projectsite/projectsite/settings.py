d5842b0e5bbcbf6a3af642854748b1f75d60838d
BL4NK-USER <202480108@psu.palawan.edu.ph>
2026-09-15 20:43:35 +0800

    login

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-w&e^ql)n$#4hn(g!g$q-!(#i^skk5pm1kf+xp$t0wzpi$dw@q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radzmar.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost', 'radzmar.pythonanywhere.com']
 
 
 # Application definition
@@ -38,6 +40,42 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hangarin',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+]
+
+if "pythonanywhere" in socket.gethostname():
+        SITE_ID = 3 # production site (psusphere.pythonanywhere.com)
+else:
+        SITE_ID = 2 # local site (127.0.0.1:8000)
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL ='/accounts/login/'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL ='/accounts/login/' # after logout, go back to login
+
+ACCOUNT_LOGOUT_REDIRECT_URL ='/'
+ACCOUNT_LOGOUT_ON_GET = True
+
+ACCOUNT_LOGIN_METHODS ={"username", "email"} # allow login with username OR email
+
+ACCOUNT_SIGNUP_FIELDS = [
+"username*",
+"email*",
+"password1*",
+"password2*",
 ]
 
 MIDDLEWARE = [
@@ -46,6 +84,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
